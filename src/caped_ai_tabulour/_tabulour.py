ae13a2fd05e13b5cb77f27eb01fbe52e0c90b60b
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-01-24 14:35:32 +0100

    set tabs

---
+++ src/caped_ai_tabulour/_tabulour.py
@@ -0,0 +1,72 @@
+from typing import List, Union
+
+import napari
+import pandas as pd
+from qtpy import QtCore, QtWidgets
+
+
+class Tabulour(QtWidgets.QTableView):
+
+    signalSelectionChanged = QtCore.Signal(object, object)
+    signalDataChanged = QtCore.Signal(str, set, pd.DataFrame)
+
+    def __init__(
+        self,
+        parent=None,
+        viewer: napari.Viewer = None,
+        layer: napari.layers.Layer = None,
+        data: pd.DataFrame = None,
+        time_key: Union[int, str] = None,
+        other_keys: List[Union[int, str]] = None,
+    ):
+
+        super().__init__(parent)
+        self._layer = layer
+        self._viewer = viewer
+        self._data = data
+        self._time_key = time_key
+        self._other_keys = other_keys
+
+        self.setSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding
+        )
+        self.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+
+        self.setSelectionBehavior(QtWidgets.QTableView.SelectRows)
+
+        # allow discontinuous selections (with command key)
+        self.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+
+        self.setSortingEnabled(True)
+
+        self._set_model()
+        # to allow click on already selected row
+        self.clicked.connect(self._on_user_click)
+
+    def rowCount(self):
+        return self._data.shape[0]
+
+    def columnCount(self):
+        return self._data.shape[1]
+
+    def _set_model(self):
+
+        self.proxy = QtCore.QSortFilterProxyModel()
+        self.proxy.setSourceModel(self._data)
+        self.setModel(self.proxy)
+        self._refreshColumns()
+
+    def _refreshColumns(self):
+
+        columns = self._data.columns
+        for column in columns:
+            colIdx = columns.get_loc(column)
+            self.setColumnHidden(colIdx, False)
+
+    def _on_user_click(self, item):
+
+        row = self.proxy.mapToSource(item).row()
+        # column = self.proxy.mapToSource(item).column()
+        if self._time_key in self._data:
+
+            self._viewer.dims.set_point(0, self._data[self._time_key][row])
